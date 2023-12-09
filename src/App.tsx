a61a46297e53036643570397eda1c4c0595d5808
Arenelin <belov.1098@mail.ru>
2023-12-09 10:53:24 +0700

    added task deletion and filtering functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {TaskType, TodoList} from './TodoList';
 
+export type FilterValuesType = 'all' | 'active' | 'completed';
+
 function App() {
-    const tasks1: Array<TaskType> = [
+
+    const [tasks, setTasks] = useState<Array<TaskType>>([
         {id: 1, title: 'CSS', isDone: true},
         {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'React', isDone: false}
-    ]
-    const tasks2: Array<TaskType> = [
-        {id: 1, title: 'Terminator', isDone: true},
-        {id: 2, title: 'XXX', isDone: false},
-        {id: 3, title: 'Jentlmens of fortune', isDone: true},
-    ]
+        {id: 3, title: 'React', isDone: false},
+        {id: 4, title: 'Redux', isDone: false},
+    ]);
+    const [filter, setFilter] = useState<FilterValuesType>('all');
+
+
+    //Если мы добавляем или удаляем задачу, то изменяем именно исходный массив,
+    //присваивая ему новое значение
+    function removeTask(id: number) {
+        const clearedTasks = tasks.filter(t => t.id !== id);
+        setTasks(clearedTasks);
+    }
+
+
+    //Фильтруя, мы не имеем права изменять исходный массив с данными!!!
+    function changeFilter(value: FilterValuesType) {
+        setFilter(value);
+    }
+
+    let tasksForTodoList: Array<TaskType> = tasks;
+    if (filter === 'active') {
+        tasksForTodoList = tasks.filter(t => !t.isDone);
+    } else if (filter === 'completed') {
+        tasksForTodoList = tasks.filter(t => t.isDone);
+    }
 
 
     return (
         <div className="App">
-            <TodoList title="What to learn" tasks={tasks1}/>
-            <TodoList title="Movies" tasks={tasks2}/>
+            <TodoList
+                title="What to learn"
+                tasks={tasksForTodoList}
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+            />
         </div>
     );
 }
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,3 +1,5 @@
+import {FilterValuesType} from './App';
+
 export type TaskType = {
     id: number
     title: string
@@ -7,6 +9,8 @@ export type TaskType = {
 type TodoListPropsType = {
     title: string
     tasks: Array<TaskType>
+    removeTask: (id: number) => void
+    changeFilter: (value: FilterValuesType) => void
 }
 
 export function TodoList(props: TodoListPropsType) {
@@ -18,14 +22,16 @@ export function TodoList(props: TodoListPropsType) {
                 <button>+</button>
             </div>
             <ul>
-                <li><input checked={props.tasks[0].isDone} type="checkbox"/>{props.tasks[0].title}<span></span></li>
-                <li><input checked={props.tasks[1].isDone} type="checkbox"/><span>{props.tasks[1].title}</span></li>
-                <li><input checked={props.tasks[2].isDone} type="checkbox"/><span>{props.tasks[2].title}</span></li>
+                {props.tasks.map(t => <li><input checked={t.isDone} type="checkbox"/>
+                        <span>{t.title}</span>
+                        <button onClick={() => props.removeTask(t.id)}>x</button>
+                    </li>
+                )}
             </ul>
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={() => props.changeFilter('all')}>All</button>
+                <button onClick={() => props.changeFilter('active')}>Active</button>
+                <button onClick={() => props.changeFilter('completed')}>Completed</button>
             </div>
         </div>
     )
