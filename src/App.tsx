10e0eeee5e0e6ce007011636f23178e52061adbd
Arenelin <belov.1098@mail.ru>
2023-12-10 08:45:04 +0700

    02 - Method Map

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import './App.css';
-import {Header} from './01 - Nested components/Header';
-import {Body} from './01 - Nested components/Body';
-import {Footer} from './01 - Nested components/Footer';
+import {NewComponent} from './02 - Method Map/NewComponent';
 
 function App() {
     return (
-        <>
-            <Header title="New Header"/>
-            <Body titleForBody="New Body"/>
-            <Footer titleForFooter="New Footer"/>
-        </>
+        <NewComponent/>
     );
 }
 
