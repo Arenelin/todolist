fd386e9d60a127330501701cb39dcd2b651b9f21
Arenelin <belov.1098@mail.ru>
2023-12-12 12:06:11 +0700

    05 - Method Filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React, {useState} from 'react';
 import './App.css';
+import {MoneyType, NewComponent} from './05 - Method Filter/NewComponent';
+
+export type FilterValueType = 'all' | 'dollars' | 'rubles'
+
 
 function App() {
-    let [a, setA] = useState(1);
+    const [money, setMoney] = useState<Array<MoneyType>>([
+        {banknote: 'dollar', nominal: 100, number: 'a123456789'},
+        {banknote: 'dollar', nominal: 50, number: 'b123456789'},
+        {banknote: 'ruble', nominal: 100, number: 'c123456789'},
+        {banknote: 'dollar', nominal: 100, number: 'd123456789'},
+        {banknote: 'dollar', nominal: 50, number: 'e123456789'},
+        {banknote: 'ruble', nominal: 100, number: 'f123456789'},
+        {banknote: 'dollar', nominal: 50, number: 'j123456789'},
+        {banknote: 'ruble', nominal: 50, number: 'h123456789'}
+    ]);
+    const [filter, setFilter] = useState<FilterValueType>('all');
 
-    const incrementData = () => {
-        setA(++a);
-        console.log(a);
-    }
-    const clearData = () => {
-        setA(0);
-    }
+    const filteredMoney = (filterValue: FilterValueType) => {
+        setFilter(filterValue);
+    };
+
+    const currentMoney = filter === 'rubles'
+        ? money.filter(b => b.banknote === 'ruble')
+        : filter === 'dollars'
+            ? money.filter(b => b.banknote === 'dollar')
+            : money
 
     return (
-        <div className="App">
-            <h1>{a}</h1>
-            <button onClick={incrementData}>+</button>
-            <button onClick={clearData}>Clear</button>
-        </div>
-
+        <NewComponent currentMoney={currentMoney} filteredMoney={filteredMoney}/>
     );
 }
 
