f05539029f2a70894419df3954022564230010dd
Arenelin <belov.1098@mail.ru>
2023-12-15 12:23:34 +0700

    added function for adding a task and clearing the input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,46 @@
 import React, {useState} from 'react';
 import './App.css';
 import {TaskType, TodoList} from './TodoList';
+import {v1} from 'uuid';
 
 export type FilterValuesType = 'all' | 'active' | 'completed';
 
 function App() {
 
-    const [tasks, setTasks] = useState<Array<TaskType>>([
-        {id: 1, title: 'CSS', isDone: true},
-        {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'React', isDone: false},
-        {id: 4, title: 'Redux', isDone: false},
+    const [tasks, setTasks] = useState<TaskType[]>([
+        {id: v1(), title: 'CSS', isDone: true},
+        {id: v1(), title: 'JS', isDone: true},
+        {id: v1(), title: 'React', isDone: false},
+        {id: v1(), title: 'Redux', isDone: false},
     ]);
     const [filter, setFilter] = useState<FilterValuesType>('all');
 
+    //Добавляем новую таску, изменяя исходный массив (ссылку (иммутабельность)) - crud-операция
+    function addTask(title: string) {
+        const newTask: TaskType = {id: v1(), title: title, isDone: false};
+        const newTasks: TaskType[] = [newTask, ...tasks];
+        setTasks(newTasks);
+    }
 
     //Если мы добавляем или удаляем задачу, то изменяем именно исходный массив,
     //присваивая ему новое значение
-    function removeTask(id: number) {
+    function removeTask(id: string) {
         const clearedTasks = tasks.filter(t => t.id !== id);
         setTasks(clearedTasks);
     }
-
 
     //Фильтруя, мы не имеем права изменять исходный массив с данными!!!
     function changeFilter(value: FilterValuesType) {
         setFilter(value);
     }
 
-    let tasksForTodoList: Array<TaskType> = tasks;
+    //Показываем те задачи из списка, которые соответствуют условию и значению фильтра
+    let tasksForTodoList: TaskType[] = tasks;
     if (filter === 'active') {
         tasksForTodoList = tasks.filter(t => !t.isDone);
     } else if (filter === 'completed') {
         tasksForTodoList = tasks.filter(t => t.isDone);
     }
-
 
     return (
         <div className="App">
@@ -43,9 +49,9 @@ function App() {
                 tasks={tasksForTodoList}
                 removeTask={removeTask}
                 changeFilter={changeFilter}
+                addTask={addTask}
             />
         </div>
     );
 }
-
 export default App;
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,37 +1,67 @@
 import {FilterValuesType} from './App';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {Button} from './Button';
 
 export type TaskType = {
-    id: number
+    id: string
     title: string
     isDone: boolean
 }
 
 type TodoListPropsType = {
     title: string
-    tasks: Array<TaskType>
-    removeTask: (id: number) => void
+    tasks: TaskType[]
+    removeTask: (id: string) => void
     changeFilter: (value: FilterValuesType) => void
+    addTask: (title: string) => void
 }
 
 export function TodoList(props: TodoListPropsType) {
+    const [newTaskTitle, setNewTaskTitleTitle] = useState('');
+    const tasksList: JSX.Element[] = props.tasks.map(t => {
+            const onRemoveHandler = () => {
+                props.removeTask(t.id)
+            }
+            return <li key={t.id}>
+                <input checked={t.isDone} type="checkbox"/>
+                <span>{t.title}</span>
+                <Button name={'x'} callback={onRemoveHandler}/>
+            </li>
+        }
+    )
+    const onNewTitleChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewTaskTitleTitle(e.currentTarget.value)
+    }
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.charCode === 13) {
+            props.addTask(newTaskTitle);
+            setNewTaskTitleTitle('');
+        }
+    }
+    const addTask = () => {
+        props.addTask(newTaskTitle);
+        setNewTaskTitleTitle('');
+    }
+    const changeFilter = (filterValue: FilterValuesType) => {
+        props.changeFilter(filterValue);
+    }
+
     return (
         <div>
             <h3>{props.title}</h3>
             <div>
-                <input/>
-                <button>+</button>
+                <input value={newTaskTitle}
+                       onChange={onNewTitleChangeHandler}
+                       onKeyPress={onKeyPressHandler}/>
+                <Button name={'+'} callback={addTask}/>
             </div>
             <ul>
-                {props.tasks.map(t => <li><input checked={t.isDone} type="checkbox"/>
-                        <span>{t.title}</span>
-                        <button onClick={() => props.removeTask(t.id)}>x</button>
-                    </li>
-                )}
+                {tasksList}
             </ul>
             <div>
-                <button onClick={() => props.changeFilter('all')}>All</button>
-                <button onClick={() => props.changeFilter('active')}>Active</button>
-                <button onClick={() => props.changeFilter('completed')}>Completed</button>
+                <Button name={'All'} callback={() => changeFilter('all')}/>
+                <Button name={'Active'} callback={() => changeFilter('active')}/>
+                <Button name={'Completed'} callback={() => changeFilter('completed')}/>
             </div>
         </div>
     )
